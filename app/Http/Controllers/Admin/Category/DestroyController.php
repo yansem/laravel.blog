dc9e7356040681d65e571e1e8d7b605c5ffa4a94
Semyon Yanichev <yas@d-element.ru>
2022-07-30 21:51:18 +0400

    rename categories to category

--- app/Http/Controllers/Admin/Category/DestroyController.php
+++ app/Http/Controllers/Admin/Category/DestroyController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Admin\Category;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Admin\Category\UpdateRequest;
 use App\Models\Category;
 
 class DestroyController extends Controller
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
     Route::group(['namespace' => 'Main'], function () {
         Route::get('/', 'IndexController');
     });
-    Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function () {
+    Route::group(['namespace' => 'Category', 'prefix' => 'category'], function () {
         Route::get('/', 'IndexController')->name('admin.category.index');
         Route::get('/create', 'CreateController')->name('admin.category.create');
         Route::post('/', 'StoreController')->name('admin.category.store');

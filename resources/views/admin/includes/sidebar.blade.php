60f142dc7e3d1697bc396ec46e0314e27052aa6c
Semyon Yanichev <yas@d-element.ru>
2022-07-30 14:00:25 +0400

    sidebar changes

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -5,12 +5,27 @@
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <li class="nav-item">
-                    <a href="{{ route('admin.categories.index') }}" class="nav-link">
-                        <i class="nav-icon far fa-image"></i>
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fa fa-box"></i>
                         <p>
                             Категории
+                            <i class="fas fa-angle-left right"></i>
                         </p>
                     </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="pages/mailbox/mailbox.html" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Все категории</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="pages/mailbox/compose.html" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Создать категорию</p>
+                            </a>
+                        </li>
+                    </ul>
                 </li>
             </ul>
         </nav>

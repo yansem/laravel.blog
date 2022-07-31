11e338bbb082e410fe6045cb6bb84dc16afe4c02
Semyon Yanichev <yas@d-element.ru>
2022-07-31 21:04:03 +0400

    posts: Service

--- app/Http/Controllers/Admin/Post/StoreController.php
+++ app/Http/Controllers/Admin/Post/StoreController.php
@@ -2,31 +2,14 @@
 
 namespace App\Http\Controllers\Admin\Post;
 
-use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\Post\StoreRequest;
-use App\Models\Post;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Storage;
 
-class StoreController extends Controller
+class StoreController extends BaseController
 {
     public function __invoke(StoreRequest $request)
     {
-        try {
-            DB::beginTransaction();
-            $data = $request->validated();
-            $tag_ids = $data['tag_ids'];
-            unset($data['tag_ids']);
-            $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
-            $data['main_image'] = Storage::disk('public')->put('/images', $data['main_image']);
-            $post = Post::firstOrCreate($data);
-            $post->tags()->attach($tag_ids);
-            DB::commit();
-        } catch (\Exception $exception) {
-            DB::rollBack();
-            abort('404');
-        }
-
+        $data = $request->validated();
+        $this->service->store($data);
         return redirect()->route('admin.post.index');
     }
 }
